5a837d4092fb15518c665c392fb342b0d93c9ef0
JRodriguezGonzalez <140106749+JRodriguezGonzalez@users.noreply.github.com>
2024-05-27 20:29:56 -0600

    commit 4
    Update css

--- monte-carlo-frontend/src/App.js
+++ monte-carlo-frontend/src/App.js
@@ -75,7 +75,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Simulación de Demanda</h1>
+        <h1>Simulación Monte Carlo</h1>
       </header>
 
       <div>
